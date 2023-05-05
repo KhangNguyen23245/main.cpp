a791c554cde8b5168892f850e3294a2b3144eda5
KhangNguyen23245 <132501453+KhangNguyen23245@users.noreply.github.com>
2023-05-05 13:54:33 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string characterName = "john";
+	int characterAge;
+	characterAge = 50;
+
+	cout << "A man name 50 "<< characterName << endl;
+	cout << "He was 59 " <<characterAge<< " years old" << endl;
+	cout << "He likes ice cream " << characterName << endl;
+	cout << "He works at a company name CF " << characterAge << endl;
+	return 0;
+}
+
+// HI friend : D
+
